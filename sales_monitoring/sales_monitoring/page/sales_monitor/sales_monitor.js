1fa4a3e2ed3fe2d8dd4c4aab4afeae38aea5fbd8
yohanneswoo10@gmail.com <yohanneswoo10@gmail.com>
2024-01-12 06:59:04 +0000

    added so item

--- sales_monitoring/sales_monitoring/page/sales_monitor/sales_monitor.js
+++ sales_monitoring/sales_monitoring/page/sales_monitor/sales_monitor.js
@@ -24,48 +24,102 @@ MyPage = Class.extend({
                 // execute methods
                 $(frappe.render_template(`
                                 <div style="text-align:center;">
+				<div id="clock" style="width: 100%;font-size: 60px;font-family: Orbitron;letter-spacing: 7px;color: #17D4FE;"></div>
                                 <div class="row">
-                                        <div class="col-md-6"style="width: 100%;" id="desc_table"></div>
-                                        <div class="col-md-6"style="width: 100%;" id="invoice"></div>
+                                        <div class="col"style="width: 100%;" id="desc_table"></div>
                                 </div>
+				<div class="row">
+					<div class="col"style="width: 100%;" id="invoice"></div>
+				</div>
+				<div class="row">
+					<div class="col"style="width: 100%;" id="so_item"></div>
+				</div>
                          </div>`, this)).appendTo(this.page.main);
                 sales_data();
-
+		show_clock();
         }
 	// end of class
 
 })
 
+let show_clock = () => {
+	var date = new Date();
+	var h = date.getHours(); // 0 - 23
+	var m = date.getMinutes(); // 0 - 59
+	var s = date.getSeconds(); // 0 - 59
+	var session = "AM";
+	if(h == 0){
+        	h = 12;
+    	}
+    
+    	if(h > 12){
+        	h = h - 12;
+        	session = "PM";
+    	}
+    
+    	h = (h < 10) ? "0" + h : h;
+    	m = (m < 10) ? "0" + m : m;
+    	s = (s < 10) ? "0" + s : s;
+    
+    	var time = h + ":" + m + ":" + s + " " + session;
+    	//document.getElementById("clock").innerText = time;
+    	//document.getElementById("clock").textContent = time;
+	document.querySelector('#clock').innerHTML = time;
+    
+    	setTimeout(show_clock, 1000);
+}
+
 let sales_data = () => {
-                setTimeout(()=>{
-                        get_sales_order();
-                        get_sales_invoice();
-                }, 100)
-        }
+	get_sales_order();
+	get_sales_invoice();
+	get_so_item();
+        setTimeout(sales_data, 1000);
+	//console.log("test");
+	/*setTimeout(()=>{
+        	get_sales_order();
+                get_sales_invoice();
+		//console.log("test");
+        }, 100)*/
+}
 
 let get_sales_order = () => {
 	frappe.call({
+			async: false,
                         method: "sales_monitoring.sales_monitoring.page.sales_monitor.sales_monitor.get_sales_order", //dotted path to server method
                         callback: function(r) {
                                         // code snippet
                                         data = r.message
-                                        console.log(r);
+                                        //console.log(r);
                                         document.querySelector('#desc_table').innerHTML = data.desctable;
                         }
         })
-
+	//setTimeout(get_sales_order, 1000);
 }
 
 let get_sales_invoice = () => {
         frappe.call({
+			async: false,
                         method: "sales_monitoring.sales_monitoring.page.sales_monitor.sales_monitor.get_sales_invoice", //dotted path to server method
                         callback: function(r) {
                                         // code snippet
                                         data = r.message
-                                        console.log(r);
+                                        //console.log(r);
                                         document.querySelector('#invoice').innerHTML = data.invoices;
                         }
         })
 
 }
 
+let get_so_item = () => {
+        frappe.call({
+                        async: false,
+                        method: "sales_monitoring.sales_monitoring.page.sales_monitor.sales_monitor.get_so_item", //dotted path to server method
+                        callback: function(r) {
+                                        // code snippet
+                                        data = r.message
+                                        //console.log(r);
+                                        document.querySelector('#so_item').innerHTML = data.soitem;
+                        }
+        })
+
+}
--- sales_monitoring/sales_monitoring/page/sales_monitor/sales_monitor.py
+++ sales_monitoring/sales_monitoring/page/sales_monitor/sales_monitor.py
@@ -6,6 +6,7 @@ global desctable
 
 @frappe.whitelist()
 def get_sales_order(**kwargs):
+	today = date.today()
 	res = frappe._dict({})
 	if not ('System Manager' in [i.role for i in frappe.get_doc('User', frappe.session.user).roles]):
 		return res
@@ -18,7 +19,8 @@ def get_sales_order(**kwargs):
 	#	as_dict=True
 	#)
 
-	doc = frappe.db.sql("""select name, customer, customer_name, transaction_date, grand_total, owner from `tabSales Order` where status = 0""", as_dict = True)
+	doc = frappe.db.sql("""select name, customer, customer_name, transaction_date, grand_total, owner, status from `tabSales Order` \
+				where status != 'Cancelled' OR transaction_date = %s""", (today), as_dict = True)
 
 	if not doc:
 		desctable = frappe.render_template("sales_monitoring/sales_monitoring/page/sales_monitor/desctable.html", {"nama": "SALES ORDER"})
@@ -26,14 +28,6 @@ def get_sales_order(**kwargs):
 		return res
 
 	desctable = frappe.render_template("sales_monitoring/sales_monitoring/page/sales_monitor/desctable.html", {"nama": "SALES ORDER", "document":doc})
-
-	#for d in doc:
-	#	desctable = frappe.render_template(
-	#		"sales_monitoring/sales_monitoring/page/sales_monitor/desctable.html",
-	#		context=dict(
-	#		customer = d.customer,
-	#		customer_name = d.customer_name,
-	#	))
 
 	res.desctable = desctable
 	return res
@@ -46,7 +40,11 @@ def get_sales_invoice(**kwargs):
 	if not ('System Manager' in [i.role for i in frappe.get_doc('User', frappe.session.user).roles]):
 		return res
 
-	doc = frappe.db.sql("""select name, customer, customer_name, posting_date, grand_total, owner from `tabSales Invoice` where posting_date = %s""", (today), as_dict = True)
+	doc = frappe.db.sql("""select t1.name as name, t1.customer as customer, t1.customer_name as customer_name, t1.posting_date as posting_date, \
+				t1.grand_total as grand_total, t1.owner as owner, t2.sales_order as sales_order \
+				from `tabSales Invoice` as t1 \
+				inner join `tabSales Invoice Item` as t2 on t2.parent = t1.name \
+				where t1.posting_date = %s""", (today), as_dict = True)
 
 	if not doc:
 		invoices = frappe.render_template("sales_monitoring/sales_monitoring/page/sales_monitor/invoices.html", {"nama": "SALES INVOICE"})
@@ -58,3 +56,26 @@ def get_sales_invoice(**kwargs):
 	res.invoices = invoices
 	return res
 
+
+@frappe.whitelist()
+def get_so_item(**kwargs):
+        today = date.today()
+        res = frappe._dict({})
+        if not ('System Manager' in [i.role for i in frappe.get_doc('User', frappe.session.user).roles]):
+                return res
+
+        doc = frappe.db.sql("""select t1.name as name, t1.customer_name as customer_name, t1.transaction_date as posting_date, t1.status, \
+                                t2.item_name as item, t2.qty as qty, t2.price_list_rate as price, t2.discount_percentage as discper, t2.discount_amount as discamt, t2.rate as harga \
+                                from `tabSales Order` as t1 \
+                                inner join `tabSales Order Item` as t2 on t2.parent = t1.name \
+                                where t1.status != 'Cancelled' AND t1.status != 'Closed' AND t1.status != 'Completed' """, as_dict = True)
+
+        if not doc:
+                soitem = frappe.render_template("sales_monitoring/sales_monitoring/page/sales_monitor/soitem.html", {"nama": "SALES ORDER ITEM"})
+                res.soitem = soitem
+                return res
+
+        soitem = frappe.render_template("sales_monitoring/sales_monitoring/page/sales_monitor/soitem.html", {"nama": "SALES ORDER ITEM", "document":doc})
+
+        res.soitem = soitem
+        return res
